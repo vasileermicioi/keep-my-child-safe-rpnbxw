fa6f9c66079231df8b55ddecad4292e50c695a21
SpeeedWagon <66512352+SpeeedWagon@users.noreply.github.com>
2021-06-26 16:36:45 +0300

    Improved UI (#9)
    * improved UI
    
    * removed comments and unused components
    
    * small improvment to alert
    
    * added child list

--- src/context/childrenDataProvider.tsx
+++ src/context/childrenDataProvider.tsx
@@ -29,10 +29,14 @@ const ChildrenDataProvider = (props: any) => {
       Name: "Mihai",
       birthdate: {
         month: 2,
-        year: 2008,
+
+        year: 2018,
+
         day: 13,
       },
       id: "0",
+      avatar:
+        "https://images.unsplash.com/photo-1519689680058-324335c77eba?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
       measurements: [
         {
           respiratoryFrequency: 100,
@@ -55,10 +59,14 @@ const ChildrenDataProvider = (props: any) => {
       Name: "Nikita",
       birthdate: {
         month: 5,
-        year: 2014,
+
+        year: 2019,
+
         day: 8,
       },
       id: "1",
+      avatar:
+        "https://images.unsplash.com/photo-1491013516836-7db643ee125a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=925&q=80",
       measurements: [
         {
           temperature: 37.7,
@@ -79,6 +87,8 @@ const ChildrenDataProvider = (props: any) => {
         day: 9,
       },
       id: "2",
+      avatar:
+        "https://images.unsplash.com/photo-1544126592-807ade215a0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
       measurements: [
         {
           temperature: 37.4,
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,7 +2,9 @@ import { IonPage, IonButton } from "@ionic/react";
 import "./Login.css";
 export const Login = () => {
   return (
+
     <IonPage>
+
       <IonButton routerLink="/children" size={"large"}>
         Login
       </IonButton>
